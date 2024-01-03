c59393de242523f64aacda58e93dc54d6c2614ad
Evgeni Dimov <edimov@plandelta.com>
2024-01-03 11:32:43 +0200

    adds workspace dependencies script

---
+++ scripts/replace_dependencies.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+const mainPackageJsonPath = './packages/react/scroll-area/package.json';
+const rootDir = './packages';
+
+const readJson = (filePath) => {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+};
+
+const writeJson = (filePath, data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+};
+
+const findPackageJsonPath = (dep) => {
+  const depName = dep.split('/')[1].replace(/^react-/, '');
+
+  const possiblePaths = [
+    path.join(rootDir, 'react', depName, 'package.json'),
+    path.join(rootDir, 'core', depName, 'package.json'),
+  ];
+  return possiblePaths.find((p) => fs.existsSync(p));
+};
+
+// Main function to replace workspace dependencies
+const replaceWorkspaceDependencies = () => {
+  const mainPackageJson = readJson(mainPackageJsonPath);
+
+  if (mainPackageJson.dependencies) {
+    Object.keys(mainPackageJson.dependencies).forEach((dep) => {
+      if (mainPackageJson.dependencies[dep].startsWith('workspace:')) {
+        const depPackageJsonPath = findPackageJsonPath(dep);
+        if (depPackageJsonPath) {
+          const depPackageJson = readJson(depPackageJsonPath);
+          mainPackageJson.dependencies[dep] = depPackageJson.version;
+        } else {
+          console.warn(`Warning: Could not find package.json for: ${dep}`);
+        }
+      }
+    });
+
+    writeJson(mainPackageJsonPath, mainPackageJson);
+    console.log('Workspace dependencies have been replaced successfully.');
+  }
+};
+
+replaceWorkspaceDependencies();
